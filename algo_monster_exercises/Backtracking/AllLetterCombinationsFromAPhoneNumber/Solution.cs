d3d316076a0f5d94b06d7e3cbbb59a941f877b63
Grzegorz Dembowski <ggdemb@gmail.com>
2023-09-05 22:13:29 +0200

    Backtracking: Generate All Letter Combinations from a Phone Number

---
+++ algo_monster_exercises/Backtracking/AllLetterCombinationsFromAPhoneNumber/Solution.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algo_monster_exercises.Backtracking.AllLetterCombinationsFromAPhoneNumber
+{
+	public class Solution
+	{
+		private readonly Dictionary<char, string> _digitsToLettersMapping = new()
+		{
+			{ '1', " " },
+			{ '2', "abc" },
+			{ '3', "def" },
+			{ '4', "ghi" },
+			{ '5', "jkl" },
+			{ '6', "mno" },
+			{ '7', "pqrs" },
+			{ '8', "tuv" },
+			{ '9', "wxyz" },
+			{ '0', "*" },
+		};
+
+		public List<string> FindLetterCombinationsOfPhoneNumber(string digits)
+		{
+			var result = new List<string>();
+			if (digits != null && digits.Length > 0)
+			{
+				var pathForRoot = "";
+				DepthFirstSearch(pathForRoot, digits, result);
+			}
+			return result;
+		}
+
+		private void DepthFirstSearch(string path, string digits, List<string> result)
+		{
+			if(digits.Length == path.Length)
+			{
+				result.Add(path);
+			}
+			else
+			{
+				var nextDigit = digits[path.Length];
+				var nextPossibleLetters = _digitsToLettersMapping[nextDigit];
+
+				foreach(var nextLetter in nextPossibleLetters)
+				{
+					path += nextLetter;
+					DepthFirstSearch(path, digits,result);
+					path = path.Remove(path.Length - 1);
+				}
+			}
+		}
+	}
+}
---
+++ algo_monster_exercises_tests/Backtracking/AllLetterCombinationsFromAPhoneNumber/SolutionTests.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+
+namespace algo_monster_exercises.Backtracking.AllLetterCombinationsFromAPhoneNumber.Tests
+{
+	public class SolutionTests
+	{
+		[Theory]
+		[InlineData("")]
+		[InlineData(null)]
+		public void FindLetterCombinationsOfPhoneNumberTestWhenEmptyOrNull(string digits)
+		{
+			var sut = new Solution();
+			var result = sut.FindLetterCombinationsOfPhoneNumber(digits);
+			Assert.Empty(result);
+		}
+		[Theory]
+		[InlineData("56", new string[] { "jm", "jn", "jo", "km", "kn", "ko", "lm", "ln", "lo" })]
+		[InlineData("235", new string[] { "adj", "adk", "adl", "aej", "aek", "ael", "afj", "afk", "afl", "bdj", "bdk", "bdl", "bej", "bek", "bel", "bfj", "bfk", "bfl", "cdj", "cdk", "cdl", "cej", "cek", "cel", "cfj", "cfk", "cfl" })]
+		public void FindLetterCombinationsOfPhoneNumberTestWhenEmpty(string digits, string[] expectedResult)
+		{
+			var sut = new Solution();
+			var result = sut.FindLetterCombinationsOfPhoneNumber(digits);
+			var resultDiff = result.Except(expectedResult);
+			var expectedResultDiff = expectedResult.Except(result);
+			Assert.Empty(resultDiff);
+			Assert.Empty(expectedResultDiff);
+
+		}
+	}
+}
