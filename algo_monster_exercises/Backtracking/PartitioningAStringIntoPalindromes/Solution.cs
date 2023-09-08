bdc0305f423fc904c53859f7f99772e111e46562
Grzegorz Dembowski <ggdemb@gmail.com>
2023-09-08 20:44:24 +0200

    Backtracking: Partitioning A String Into Palindromes

---
+++ algo_monster_exercises/Backtracking/PartitioningAStringIntoPalindromes/Solution.cs
@@ -0,0 +1,48 @@
+﻿namespace algo_monster_exercises.Backtracking.PartitioningAStringIntoPalindromes
+{
+	public class Solution
+	{
+		public List<List<string>> GetAllPalindromes(string textToCheck)
+		{
+			var finalResult = new List<List<string>>();
+			if (!string.IsNullOrEmpty(textToCheck))
+				DepthFirstSearch(finalResult, localState: new List<string>(), textToCheck, startIndex: 0);
+			return finalResult;
+		}
+
+		private void DepthFirstSearch(List<List<string>> finalResult, List<string> localState, string textToCheck, int startIndex)
+		{
+			if (startIndex == textToCheck.Length)
+			{
+				finalResult.Add(new List<string>(localState));
+				return;
+			}
+
+			for (int currentIndex = startIndex; currentIndex < textToCheck.Length; currentIndex++)
+			{
+				var stringToCheck = textToCheck.Substring(startIndex, currentIndex - startIndex + 1);
+				if (IsPalindrome(stringToCheck))
+				{
+					localState.Add(stringToCheck);
+					DepthFirstSearch(finalResult, localState, textToCheck, currentIndex + 1);
+					localState.RemoveAt(localState.Count - 1);
+				}
+			}
+		}
+
+		private bool IsPalindrome(string textToCheck)
+		{
+			var l = 0;
+			var r = textToCheck.Length - 1;
+
+			while (l < r)
+			{
+				if (textToCheck[l] != textToCheck[r])
+					return false;
+				l++;
+				r--;
+			}
+			return true;
+		}
+	}
+}
---
+++ algo_monster_exercises_tests/Backtracking/PartitioningAStringIntoPalindromes/SolutionTests.cs
@@ -0,0 +1,41 @@
+﻿using Xunit;
+
+namespace algo_monster_exercises.Backtracking.PartitioningAStringIntoPalindromes.Tests
+{
+	public class SolutionTests
+	{
+		[Theory()]
+		[InlineData("")]
+		[InlineData(null)]
+		public void GetAllPalindromesTestWhenThereIsNoText(string textToCheck)
+		{
+			var sut = new Solution();
+			var result = sut.GetAllPalindromes(textToCheck);
+			Assert.Empty(result);
+		}
+		[Fact()]
+		public void GetAllPalindromesTestForRegularText()
+		{
+			var sut = new Solution();
+			var result = sut.GetAllPalindromes(textToCheck: "aab");
+			Assert.Equal("a", result[0][0]);
+			Assert.Equal("a", result[0][1]);
+			Assert.Equal("b", result[0][2]);
+			Assert.Equal("aa", result[1][0]);
+			Assert.Equal("b", result[1][1]);
+		}
+
+		[Fact()]
+		public void GetAllPalindromesTestWhenThereAreOnlyOneLetterPalindromes()
+		{
+			var sut = new Solution();
+			var result = sut.GetAllPalindromes(textToCheck: "abcdef");
+			Assert.Equal("a", result[0][0]);
+			Assert.Equal("b", result[0][1]);
+			Assert.Equal("c", result[0][2]);
+			Assert.Equal("d", result[0][3]);
+			Assert.Equal("e", result[0][4]);
+			Assert.Equal("f", result[0][5]);
+		}
+	}
+}
